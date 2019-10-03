7d66eea88fd5a99b8543b7452ad8f54b323ddbc7
Bo Thompson <ethompson@godaddy.com>
2019-10-02 20:25:15 -0700

    client improvements

--- cmd/call.go
+++ cmd/call.go
@@ -64,7 +64,8 @@ func init() {
 	callCmd.Flags().StringVarP(&clientOptions.Host, "host", "H", clientOptions.Host, "The target host")
 	callCmd.Flags().IntVarP(&clientOptions.Port, "port", "p", clientOptions.Port, "The port the target host is listening on")
 	callCmd.Flags().BoolVar(&clientOptions.UseWebSocket, "ws", false, "Use the websocket protocol will be used for server communications")
-	callCmd.Flags().IntVarP(&clientOptions.IntervalSeconds, "interval", "i", 0, "if interval is greater than 0, requests will be sent continuously spaced at specified intervals in seconds. When used in conjuction with the --ws flag, a single websocket connection will be used for all writes")
+	callCmd.Flags().IntVarP(&clientOptions.IntervalSeconds, "interval", "i", 0, "If interval is greater than 0, requests will be sent continuously spaced at specified intervals in seconds. When used in conjuction with the --ws flag, a single websocket connection will be used for all writes")
+	callCmd.Flags().IntVarP(&clientOptions.TimeoutSeconds, "timeout", "t", 5, "Amount of time (in seconds) to wait for client requests")
 	callCmd.Flags().StringP("data", "d", "", "Data to send to the target web server")
 	callCmd.Flags().BoolVarP(&clientOptions.ExitMode, "exit", "e", false, "Exit immediately if request ends in an error or non 2XX status code")
 }
